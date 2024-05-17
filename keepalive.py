30c0ba438a2087f7fef06616a00b50c32eb02404
ayuhays <164576111+ayuhays@users.noreply.github.com>
2024-05-17 23:26:57 +0300

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -5,11 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "Bot is alive!"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    server = Thread(target=run)
+    server.start()
