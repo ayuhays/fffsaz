f6738a84a3ff184f3deadbec062cbb9bb1692fab
ayuhays <164576111+ayuhays@users.noreply.github.com>
2024-05-18 23:05:39 +0300

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -1,6 +1,14 @@
-const http = require("http")
+// keepalive.js
 
-http.createServer(function(req,res) {
-  res.write("Alive")
-  res.end()
-}).listen(8080)
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Discord self-bot is running!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Keepalive server is running on port ${PORT}`);
+});
