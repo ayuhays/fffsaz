8235a4c6a149743dfd20bcfd0cc21578ebb87498
ayuhays <164576111+ayuhays@users.noreply.github.com>
2024-05-18 23:33:29 +0300

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -1,11 +1,11 @@
-// selfbot.js
+/ selfbot.js
 
 const { exec } = require('child_process');
 
 const channelId = process.env.CHANNEL_ID;
 const token = process.env.DISCORD_TOKEN;
 
-const command = `ffmpeg -f lavfi -i anullsrc -ar 44100 -ac 2 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le pipe:1 > /dev/null`;
+const command = `ffmpeg -f lavfi -i anullsrc -ar 44100 -ac 2 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le -i /dev/zero -acodec pcm_s16le -ac 2 -ar 48000 -f s16le pipe:1 > /dev/null`;
 
 exec(command, (error, stdout, stderr) => {
     if (error) {
